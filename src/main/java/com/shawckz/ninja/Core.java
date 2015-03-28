28b98dac620770bd77ba00e80b6095b409a79b93
CodeMyAss <codemyass@gmail.com>
2015-03-27 21:49:30 -0700

    Update Core.java

--- src/main/java/com/shawckz/ninja/Core.java
+++ src/main/java/com/shawckz/ninja/Core.java
@@ -47,7 +47,7 @@ public class Core extends JavaPlugin {
 
     @Override
     public void onDisable(){
-        log("You can't shut us down! Huehuehuehue");
+        log("You can't shut us down! YOU SHIT HAS BEEN JACKED THX CRUSADER @ PINGD3MSQUAD");
     }
 
     public static Plugin getPlugin() {
